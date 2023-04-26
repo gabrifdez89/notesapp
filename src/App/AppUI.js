201837232a6c6e4fe66548847e30ddce66eaa396
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2023-04-26 12:08:53 +0200

    Using React.useContext

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,28 +6,30 @@ import { NoteItem } from '../NoteItem';
 import { CreateNoteButton } from '../CreateNoteButton';
 
 function AppUI() {
+    const { 
+        notes,
+        createNote,
+        deleteNote,
+    } = React.useContext(NotesContext);
+
     return (
-        <NotesContext.Consumer>
-            {({ notes, createNote, deleteNote }) => (
-                <React.Fragment>
-                    <AppBox>
-                        <NotesList>
-                            {notes.map(note => (
-                                <NoteItem
-                                    title={note.title}
-                                    text={note.text}
-                                    key={note.id}
-                                    onCrossMarkClick={() => deleteNote(note.id)}
-                                />
-                            ))}
-                        </NotesList>
-                        <CreateNoteButton
-                            onClick={() => createNote()}
+        <React.Fragment>
+            <AppBox>
+                <NotesList>
+                    {notes.map(note => (
+                        <NoteItem
+                            title={note.title}
+                            text={note.text}
+                            key={note.id}
+                            onCrossMarkClick={() => deleteNote(note.id)}
                         />
-                    </AppBox>
-                </React.Fragment>
-            )}
-        </NotesContext.Consumer>
+                    ))}
+                </NotesList>
+                <CreateNoteButton
+                    onClick={() => createNote()}
+                />
+            </AppBox>
+        </React.Fragment>
     );
 }
 
