9368d7ef3ca252e7000179eb6958d85c92862be6
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2023-05-08 21:06:26 +0200

    Refactoring to use reducer

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,22 +1,41 @@
 import React from 'react';
 
 function useLocalStorage(itemName, defaultItem) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  const [state, dispatch] = React.useReducer(reducer, initialState({ defaultItem }));
 
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(defaultItem));
-    parsedItem = defaultItem;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+  const {
+    item,
+  } = state;
 
-  const [item, setItem] = React.useState(parsedItem);
+  // ACTION CREATORS
+  const onSave = (newItem) => dispatch({
+    type: actionTypes.save,
+    payload: newItem,
+  });
+
+  const onInitialLoad = (initialLoad) => dispatch({
+    type: actionTypes.initialLoad,
+    payload: initialLoad,
+  });
+
+  React.useEffect(() => {
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
+
+    if (!localStorageItem) {
+      localStorage.setItem(itemName, JSON.stringify(defaultItem));
+      parsedItem = defaultItem;
+    } else {
+      parsedItem = JSON.parse(localStorageItem);
+    }
+
+    onInitialLoad(parsedItem);
+  }, [defaultItem, itemName]);
 
   const saveItem = (newItem) => {
     const stringifiedItem = JSON.stringify(newItem);
     localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    onSave(newItem);
   };
 
   return { 
@@ -24,5 +43,29 @@ function useLocalStorage(itemName, defaultItem) {
     saveItem,
   };
 }
+
+const initialState = ({ defaultItem }) => ({
+  item: defaultItem,
+});
+
+const actionTypes = {
+  save: 'SAVE',
+  initialLoad: 'INITIAL_LOAD',
+};
+
+const reducerObject = (state, payload) => ({
+  [actionTypes.save]: {
+    ...state,
+    item: payload,
+  },
+  [actionTypes.initialLoad]: {
+    ...state,
+    item: payload,
+  }
+});
+
+const reducer = (state, action) => {
+  return reducerObject(state, action.payload)[action.type] || state;
+};
 
 export { useLocalStorage };
